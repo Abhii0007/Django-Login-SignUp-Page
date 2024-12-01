abbb7aa5f73b239e8b6b81fdbc7214e7eb18de2f
Abhishek Verma <113354749+Abhii0007@users.noreply.github.com>
2024-12-01 11:04:39 +0530

    Update database_show.py

--- database_show.py
+++ database_show.py
@@ -1,15 +1,16 @@
-import os
-import django
-
+import os,django,time
+import pandas
 # Set the correct settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'login.settings')
 django.setup()
-
+   
 from login.models import User
 
-# Fetch all user data
-users = User.objects.all()
+users = User.objects.values()
+#for user in users:
+#    print(f"Name: {user.name}, Phone: {user.phone}, Email: {user.email},password:{user.password}")
 
-# Print user data
-for user in users:
-    print(f"Name: {user.name}, Phone: {user.phone}, Email: {user.email},password:{user.password}")
+
+
+print(pandas.DataFrame(list(users)))
+
