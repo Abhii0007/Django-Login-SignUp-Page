073f4a0791895ec1a55d6ea2050aadf211c263a5
Abhishek Verma <113354749+Abhii0007@users.noreply.github.com>
2024-12-01 11:19:03 +0530

    test

--- database_show.py
+++ database_show.py
@@ -7,10 +7,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'login.settings')
 django.setup()
 
 
-
 users = User.objects.values()
-#for user in users:
-#    print(f"Name: {user.name}, Phone: {user.phone}, Email: {user.email},password:{user.password}")
 
 print(pandas.DataFrame(list(users)))
 
+
+#for user in users:
+#    print(f"Name: {user.name}, Phone: {user.phone}, Email: {user.email},password:{user.password}")
