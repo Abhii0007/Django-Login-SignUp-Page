16cd9e4ce61c3b4b57fa93826e58a0a74f37a2f2
Adiitya003 <adityasonofmj003@gmail.com>
2024-12-04 10:44:28 +0530

    Update database_show.py

--- database_show.py
+++ database_show.py
@@ -1,12 +1,12 @@
 import os,django,time
 import pandas
-from login.models import User
+
 
 # Set the correct settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'login.settings')
 django.setup()
 
-
+from login.models import User
 users = User.objects.values()
 #for user in users:
 #    print(f"Name: {user.name}, Phone: {user.phone}, Email: {user.email},password:{user.password}")
